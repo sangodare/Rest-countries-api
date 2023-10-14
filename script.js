b5b157216dcf04c765cb2d16fd3aa8a37fb49e18
sangodare <sangodare.toyeeb@gmail.com>
2023-10-14 18:27:07 +0100

    homepage countries added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const themeText = document.querySelector('.theme-text');
 const themeIcon = document.querySelector('.theme-icon');
 const filterCountries = document.querySelector('.filter-countries');
 const regions = document.querySelector('.regions');
+const countriesContainer = document.querySelector('.countries-container');
 
 
 //functions
@@ -27,7 +28,62 @@ function displayRegions() {
     regions.classList.toggle('hidden');
 }
 
+function renderCountries(data) {
+
+    const markup = `
+        <li class="country">
+            <img class="flag" src="${data[0].flags.png}" alt="${data[0].name.common}'s flag">
+            <div class="country-details">
+                <h1 class="country-name">${data[0].name.common}</h1>
+                <div>
+                    <span>Population:</span>
+                    <span>${new Intl.NumberFormat().format(data[0].population)}</span>
+                </div>
+                <div>
+                    <span>Region:</span>
+                    <span>${data[0].region}</span>
+                </div>
+                <div>
+                    <span>Capital:</span>
+                    <span>${data[0].capital}</span>
+                </div>
+            </div>
+        </li>
+    `;    
+    countriesContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+async function getCountryData(country, errorMsg = 'Reload the page') {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    if (!response.ok) throw new Error(errorMsg);
+    return await response.json();
+
+}
+
+async function loadCountries() {
+    try {
+        const data = await Promise.all([
+            getCountryData('germany'),
+            getCountryData('united states of america'),
+            getCountryData('brazil'),
+            getCountryData('iceland'),
+            getCountryData('afghanistan'),
+            getCountryData('Australia'),
+            getCountryData('albania'),
+            getCountryData('algeria')
+        ]);
+        data.map(countryData => renderCountries(countryData));
+        console.log(data);
+        data.map(data => console.log(data[0].flags.png));
+
+    } catch(err) {
+        console.log(err);
+    }
+    
+}
+
 //event listeners
 
 theme.addEventListener('click', changeTheme);
 filterCountries.addEventListener('click', displayRegions);
+document.addEventListener('DOMContentLoaded', loadCountries);
